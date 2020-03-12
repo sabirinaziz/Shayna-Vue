2849f496f7c1f43e826df01bbd7dd4dc5bfa4322
sabirinaziz <azizsabirin98@gmail.com>
2020-03-12 18:20:23 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <HeroShayna/>
+    <WomenBannerShayna/>
+    <InstagramShayna/>
+    <PartnerLogoShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomenBannerShayna from '@/components/WomenBannerShayna.vue';
+import InstagramShayna from '@/components/InstagramShayna.vue';
+import PartnerLogoShayna from '@/components/PartnerLogoShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenBannerShayna,
+    InstagramShayna,
+    PartnerLogoShayna,
+    FooterShayna
+  }
+}
+</script>
